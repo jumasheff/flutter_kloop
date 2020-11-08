03965b7bd17c49bc7ea62286514320491c20c6cc
muratjumashev <jumasheff@gmail.com>
2020-11-08 23:30:40 +0600

    Add actual page fetching to getArticleByUrl

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kloop/app/data/model/articles.dart';
 import 'package:flutter_kloop/app/routes/app_pages.dart';
+import 'package:flutter_kloop/app/utils/request.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -22,13 +23,16 @@ class HomeController extends GetxController {
     super.onClose();
   }
 
-  void getArticleByUrl() {
+  void getArticleByUrl() async {
     article = ArticleModel(
       "Some title",
       "Анын ою боюнча, ЖЭБ дагы, жеке үйлөрдүн меши деле мынча кара түтүн пайда кыла алмак эмес, ал эми кара түтүндүн булагы болуп айылдардын айланасындагы өрттөлгөн аянттар эсептелет.",
       ['Article authors'],
       DateTime.now(),
     );
+    Request request = Request(url: this.urlTextController.text);
+    var data = await request.get();
+    print(data.body);
     Get.toNamed(Routes.ARTICLE);
   }
 }
